b2ff0b8523ee186bc50320a3c3c16141c1882cdd
aidend <aidendawes@gmail.com>
2021-04-05 10:31:05 +0200

    payment and keygen working

--- functions/index.js
+++ functions/index.js
@@ -10,18 +10,20 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const serviceAccount = require('./amcham-app-firebase-adminsdk-j7px6-82dff37ac5.json');
 const cors = require('cors')({ origin: true });
+const https = require('https');
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.firestore();
 
 exports.registerUser = functions.https.onRequest((req, res) => {
     cors(req, res, async function () {
         // getting dest email by query string
-        const userHash = req.query.user;
+        const userKey = req.query.user;
         const eventID = req.query.eventID;
         var data;
-        console.log(userHash);
-        var snapshot = await db.collection('UserInfo').where('hash', '==', userHash.toString()).get();
+        console.log(userKey);
+        var snapshot = await db.collection('UserInfo').where('key', '==', userKey.toString()).get();
         var user = snapshot.docs[0].id;
+        console.log(user);
         //update user field
         var userEventData = [];
         await db.collection('UserInfo').doc(user).get().then(documentSnapshot => {
@@ -54,30 +56,36 @@ exports.registerUser = functions.https.onRequest((req, res) => {
             eventUserData = [user];
         }
         await db.collection('Events').doc(eventID).set({ 'registered_users': eventUserData}, {merge: true});
-
-
-        res.send('success');
-        res.send('you can close this window');
+        //var userEmailString; // make it the email with %40
+        //await https.get('https://us-central1-amcham-app.cloudfunctions.net/genkey?user='+userEmailString);
+        await updatekey(user);
+        res.send('success \n you can close this window');
 
         return;
 
     });
 });
-exports.hash = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-        const input = req.query.input;
-        var v = djb2_xor(input);
-        return res.send(v.toString());
+exports.genkey = functions.https.onRequest((req, res) => {
+    cors(req, res, async function () {
+        const user = req.query.user;
+        var key = await updatekey(user); 
+        return res.send(key);
 
     });
 });
 
-function djb2_xor(str) {
-    let len = str.length
-    let h = 5381
-   
-    for (let i = 0; i < len; i++) {
-      h = h * 33 ^ str.charCodeAt(i)
-     }
-     return h >>> 0
-   }
+
+   function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ async function updatekey(user){
+    var key = makeid(15);
+    db.collection('UserInfo').doc(user).set({'key':key.toString()}, {merge:true});
+    return key;
+ }
